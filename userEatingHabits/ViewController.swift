b7e04f3d9acec2571cde095f120086d551dbc663
John Zheng <amayzheng@d142-058-171-246.wireless.sfu.ca>
2018-06-25 18:59:22 -0700

    Initial Commit

---
+++ userEatingHabits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  userEatingHabits
+//
+//  Created by John Zheng on 2018-06-25.
+//  Copyright © 2018 John Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
